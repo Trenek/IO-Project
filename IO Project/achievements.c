8ea6177ecaecf8e2d24c3f0f6f5e52fab2136194
Trenek <mateusz.sas@vp.pl>
2024-05-06 08:43:19 +0200

    addition of missing things

--- IO Project/achievements.c
+++ IO Project/achievements.c
@@ -8,7 +8,7 @@
 #define INC_X (10)
 #define FONT_SIZE (25)
 
-void achievements(enum state* state, Font fonts[]) {
+void achievements(enum state *state, Font fonts[]) {
     const int height = GetScreenHeight() >> 1;
     const int spaceY = INC_Y + INC_Y + FONT_SIZE + 10;
 
@@ -16,6 +16,19 @@ void achievements(enum state* state, Font fonts[]) {
     Color color2 = { .r = 78, .g = 215, .b = 50, .a = 255 };
     Color color3 = { .r = 78, .g = 215, .b = 50, .a = 105 };
 
+    struct button menuTitle = {
+        .text = "Osiągnięcia",
+        .x = GetScreenWidth() >> 1,
+        .y = 100,
+        .incX = 0,
+        .incY = 0,
+        .font = &fonts[0],
+        .fontSize = 100,
+        .fontColor = BLACK,
+        .color = BLANK,
+        .hoverColor = BLANK,
+        .spaceing = 0
+    };
     struct button goBack = {
         .text = "Powrót",
         .x = GetScreenWidth() >> 1,
@@ -32,9 +45,10 @@ void achievements(enum state* state, Font fonts[]) {
 
     while (!WindowShouldClose() && *state == ACHIEVEMENTS) {
         BeginDrawing();
-        ClearBackground(color);
+            ClearBackground(color);
 
-        DrawButton(goBack);
+            DrawButton(menuTitle);
+            DrawButton(goBack);
         EndDrawing();
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
