1a2fedfd27706838cd37f005a86577c181c7a4d4
MSkrobot <Michal.skrobot@student.uj.edu.pl>
2024-05-07 19:46:54 +0200

    Added load game

--- IO Project/loadGame.c
+++ IO Project/loadGame.c
@@ -1,49 +1,74 @@
 #include <raylib.h>
 
 #include "state.h"
-#include "menuInfo.h"
 
-//#define FULLSCREEN
+#include "button.h"
 
-int main(void) {
-    enum state state = MENU;
-    void (*function[])(enum state* state, Font fonts[]) = {
-        [MENU] = menu,
-        [NEW_GAME] = newGame,
-        [LOAD_GAME] = loadGame,
-        [SETTINGS] = settings,
-        [ACHIEVEMENTS] = achievements,
-        [PLAY] = play
+#define INC_Y (10)
+#define INC_X (10)
+#define FONT_SIZE (25)
+
+void loadGame(enum state* state, Font fonts[]) {
+    const int height = GetScreenHeight() >> 1;
+    const int spaceY = INC_Y + INC_Y + FONT_SIZE + 10;
+    const int spaceX = 90;
+
+    Color color = { .r = 100, .g = 100, .b = 100, .a = 255 };
+    Color color2 = { .r = 78, .g = 215, .b = 50, .a = 255 };
+    Color color3 = { .r = 78, .g = 215, .b = 50, .a = 105 };
+
+    struct button title = {
+        .text = "Wczytywanie gry",
+        .x = GetScreenWidth() >> 1,
+        .y = 100,
+        .incX = 0,
+        .incY = 0,
+        .font = &fonts[0],
+        .fontSize = 100,
+        .fontColor = BLACK,
+        .color = BLANK,
+        .hoverColor = BLANK,
+        .spaceing = 0
+    };
+    struct button loadGame = {
+        .text = "Wczytaj",
+        .x = (GetScreenWidth() >> 1) - spaceX,
+        .y = height + 4 * spaceY,
+        .incX = INC_X,
+        .incY = INC_Y,
+        .font = &fonts[0],
+        .fontSize = FONT_SIZE,
+        .fontColor = BLACK,
+        .color = color2,
+        .hoverColor = color3,
+        .spaceing = 0
+    };
+    struct button goBack = {
+        .text = "Powrót",
+        .x = (GetScreenWidth() >> 1) + spaceX,
+        .y = height + 4 * spaceY,
+        .incX = INC_X,
+        .incY = INC_Y,
+        .font = &fonts[0],
+        .fontSize = FONT_SIZE,
+        .fontColor = BLACK,
+        .color = color2,
+        .hoverColor = color3,
+        .spaceing = 0
     };
 
-    do {
-        state = MENU;
+    while (!WindowShouldClose() && *state == LOAD_GAME) {
+        BeginDrawing();
+        ClearBackground(color);
 
-        SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
-#ifdef FULLSCREEN
-        int display = GetCurrentMonitor();
-        InitWindow(GetMonitorWidth(display), GetMonitorHeight(display), "Project");
-        ToggleFullscreen();
-#else
-        InitWindow(800, 600, "Project");
-#endif
+        DrawButton(title);
+        DrawButton(loadGame);
+        DrawButton(goBack);
+        EndDrawing();
 
-        SetTargetFPS(240);
-
-        Font fonts[] = {
-            LoadFontEx("resources/fonts/font2.ttf", 100, NULL, 512)
-        };
-
-        SetFontsFilter(fonts, sizeof(fonts) / sizeof(Font));
-
-        while (!WindowShouldClose() && (state != EXIT) && (state != RELOAD)) {
-            function[state](&state, fonts);
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            if (isMouseOver(loadGame)) *state = LOAD_GAME;
+            else if (isMouseOver(goBack)) *state = MENU;
         }
-
-        UnloadFonts(fonts, sizeof(fonts) / sizeof(Font));
-
-        CloseWindow();
-    } while (state == RELOAD);
-
-    return 0;
+    }
 }
