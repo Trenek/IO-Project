1271e2644725966559624c569d082958cd70d378
marekk42 <168592339+marekk42@users.noreply.github.com>
2024-05-07 11:53:33 +0200

    enable settings

--- IO Project/IO Project.c
+++ IO Project/IO Project.c
@@ -11,7 +11,7 @@ int main(void) {
         [MENU] = menu,
         [NEW_GAME] = newGame,
         //[LOAD_GAME] = loadGame,
-        //[SETTINGS] = settings,
+        [SETTINGS] = settings,
         [ACHIEVEMENTS] = achievements,
         [PLAY] = play
     };
