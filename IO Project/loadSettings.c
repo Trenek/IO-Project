710611c67ce4c44dbceee58ab7b449a7b7393991
Trenek <mateusz.sas@vp.pl>
2024-05-09 23:48:50 +0200

    minor modification

--- IO Project/loadSettings.c
+++ IO Project/loadSettings.c
@@ -3,7 +3,12 @@
 //#define FULLSCREEN
 
 void loadSettings(void) {
-    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT);
+    SetConfigFlags(
+//      FLAG_WINDOW_UNDECORATED |
+        FLAG_WINDOW_HIGHDPI | 
+        FLAG_MSAA_4X_HINT | 
+        FLAG_VSYNC_HINT
+    );
 #ifdef FULLSCREEN
     int display = GetCurrentMonitor();
     InitWindow(GetMonitorWidth(display), GetMonitorHeight(display), "Project");
