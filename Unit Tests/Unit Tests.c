4c44d745b1833434aa15194b0dbaeda777288338
Trenek <mateusz.sas@vp.pl>
2024-06-07 22:37:00 +0200

    moved setup up

--- Unit Tests/Unit Tests.c
+++ Unit Tests/Unit Tests.c
@@ -3,10 +3,11 @@
 #include "tests.h"
 
 int main(void) {
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+
     if (CUE_SUCCESS == CU_initialize_registry())
     if (CUE_SUCCESS == add_tests_achievementElem())
     if (CUE_SUCCESS == add_tests_button()) {
-        CU_basic_set_mode(CU_BRM_VERBOSE);
         CU_basic_run_tests();
     }
 
